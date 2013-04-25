f2066108c0c58ca751ff9dd7f32b7617c1509099
Sören Link (Macbook) <linksoeren@googlemail.com>
2013-04-25 17:34:43 +0200

    multiline parsing with comments sorta works

--- lib/ksp-cfg/parser/cfg.rb
+++ lib/ksp-cfg/parser/cfg.rb
@@ -1,37 +1,48 @@
 module KspCfg
   module Parser
     class Cfg < Parslet::Parser
-      # Single character rules
-      rule(:pair) {
-        key >> value
-      }
+      rule(:space)      { match["\t "] }
+      rule(:space?)     { space.repeat }
+      rule(:newline)    { str("\n") }
 
-      rule(:key) {
-        spaces? >>
-        (match('[A-Za-z_-]') >> match('[A-Za-z0-9_-]').repeat).repeat.as(:key) >>
-        spaces? >> str("=")
-      }
+      rule(:comment?) do
+        (str("//") >> (newline.absent? >> any).repeat).maybe
+      end
 
-      rule(:value) {
-        spaces? >>
-        (match('[A-Za-z_-]') >> match('[A-Za-z0-9_-]').repeat).repeat.as(:value) >>
-        spaces?
-      }
+      rule(:string) do
+        str("//").absent? >> match('[A-Za-z_-]') >> match('[ A-Za-z0-9_-]').repeat
+      end
 
-      rule(:spaces) {
-        match('\s').repeat(1)
-      }
-      rule(:spaces?) {
-        spaces.maybe
-      }
+      rule :key do
+        str("//").absent? >> newline.absent? >>
+        (space.absent? >> any).repeat(1).as(:key)
+      end
 
-      rule(:newline) {
-        spaces? >> match('\n').repeat(1)
-      }
+      rule :value do
+        string.as(:value)
+      end
 
-      rule(:pair_list) { pair >> pair.repeat.maybe }
-      root :pair_list
+      rule :assignment do
+        key >>
+        space? >> str("=") >> space? >>
+        value
+      end
+
+      rule :assignment_line do
+        space? >> assignment.maybe >> space? >> comment?
+      end
+
+      rule :assignments do
+        assignment_line >> (newline >> assignment_line.maybe).repeat.maybe
+      end
+
+      rule :document do
+        (assignment >>
+         key_group.repeat >>
+         newline.maybe).as(:document)
+      end
+
+      root :document
     end
-
   end
 end
--- spec/parser/cfg_spec.rb
+++ spec/parser/cfg_spec.rb
@@ -2,16 +2,8 @@ require_relative "../../lib/ksp-cfg"
 
 describe KspCfg::Parser::Cfg do
   let(:parser) { KspCfg::Parser::Cfg.new }
-  it "matches a key" do
-    parser.key.parse('some_key =').should == { key: 'some_key' }
-  end
-
-  it "matches a value" do
-    parser.value.parse('some_crazy_value').should == { value: 'some_crazy_value' }
-  end
-
   it "matches a pair" do
-    parser.pair.parse('some_key = some_value').should == {
+    parser.assignment_line.parse('some_key = some_value').should == {
       key: 'some_key',
       value: 'some_value'
     }
@@ -22,12 +14,38 @@ describe KspCfg::Parser::Cfg do
       key1 = value1
       key2 = value2
     EOT
-    parser.pair_list.parse( content ).should == [ {
+    parser.assignments.parse( content ).should == [ {
       key: 'key1',
       value: 'value1'
     }, {
       key: 'key2',
       value: 'value2'
     }]
+  end
+
+  it "matches a pair_list with whitespaces" do
+    content = <<-EOT.gsub(/^ {6}/, '')
+      key1 = value1
+      key2 = va lu e2
+    EOT
+    parser.assignments.parse( content ).should == [ {
+      key: 'key1',
+      value: 'value1'
+    }, {
+      key: 'key2',
+      value: 'va lu e2'
+    }]
+  end
+
+
+  it "ignores commented lines" do
+    content = <<-EOT.gsub(/^ {6}/, '')
+      // key1 = value1
+      key2 = value2
+    EOT
+    parser.assignments.parse( content ).should == {
+      key: 'key2',
+      value: 'value2'
+    }
   end
 end
