7d28fb5935a253cc3c540493c61c06eff879a8e2
Sören Link (Macbook) <linksoeren@googlemail.com>
2013-04-26 11:33:24 +0200

    Cleanup after value_list parsing commit

--- lib/ksp-cfg/parser/cfg.rb
+++ lib/ksp-cfg/parser/cfg.rb
@@ -67,23 +67,12 @@ module KspCfg
       ###############################################################
       # ASSIGNMENTS
       rule :assignment do
-        spaced(key) >> str("=") >> (spaced(list) | spaced(value))
+        spaced(key) >> str("=") >> (spaced(list))
       end
 
       # Values can be any data type, keys only strings without spaces
       rule(:value) do
         ( float | integer | boolean | string )
-      end
-
-      def value_list(value_type)
-        value_type >>
-        (space? >> str(",") >> space? >> value_type).repeat >>
-        space? >> str(",").maybe
-      end
-
-      def list_contents
-        value_list(float) | value_list(integer) |
-          value_list(boolean) | value_list(string)
       end
 
       rule :list do
